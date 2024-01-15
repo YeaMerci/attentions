720cfcb7c13df811b03650940f4a9ea8c60024ae
YeaMerci <113313229+YeaMerci@users.noreply.github.com>
2024-01-16 01:14:59 +0300

    ref: updated imports & added docs in layers package

--- attentions/layers/__init__.py
+++ attentions/layers/__init__.py
@@ -1,2 +1,9 @@
-from .attend_linear import *
+"""
+This package implements any layers with attention mechanism
+which can be used for building custom models.
+"""
+
 from .mha import *
+from .transformer import *
+from .attend_linear import *
+
